604adcf309b2cfa46ff95e806c09efd18d73bd25
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-04-08 19:18:16 +0530

    src

---
+++ src/video/state/providers/providerTypes.ts
@@ -0,0 +1,31 @@
+import { MWStreamQuality, MWStreamType } from "@/backend/helpers/streams";
+
+type VideoPlayerSource = {
+  source: string;
+  type: MWStreamType;
+  quality: MWStreamQuality;
+  providerId?: string;
+  embedId?: string;
+} | null;
+
+export type VideoPlayerStateController = {
+  pause: () => void;
+  play: () => void;
+  setSource: (source: VideoPlayerSource) => void;
+  setTime(time: number): void;
+  setSeeking(active: boolean): void;
+  exitFullscreen(): void;
+  enterFullscreen(): void;
+  setVolume(volume: number): void;
+  startAirplay(): void;
+  setCaption(id: string, url: string): void;
+  clearCaption(): void;
+  getId(): string;
+  togglePictureInPicture(): void;
+};
+
+export type VideoPlayerStateProvider = VideoPlayerStateController & {
+  providerStart: () => {
+    destroy: () => void;
+  };
+};
